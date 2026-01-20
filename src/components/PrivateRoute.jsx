8acc789db0897097cc81fb295626c87deb605da1
Mhasan9927 <s20502002649@nu.ac.bd>
2026-01-20 20:38:01 +0600

    fixed login issue

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,12 +1,23 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import AuthContext from "../Authentication/Authcontext";
+import Swal from "sweetalert2";
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
-
   if (loading)
     return <p className="h-screen justify-center items-center">Loading...</p>;
-  return user ? children : <Navigate to="/login" />;
+
+  if (!user) {
+    Swal.fire({
+      icon: "warning",
+      title: "Access Denied",
+      text: "Please log in to continue",
+      confirmButtonColor: "#D4AF37",
+    });
+
+    return <Navigate to="/login" />;
+  }
+  return children;
 };
 export default PrivateRoute;
